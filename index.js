aa7e0628bfac2b61001dacf41866f7828829012d
Min-Tri <tmtri.290798@gmail.com>
2026-01-04 17:58:48 +0700

    test

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
@@ -25,15 +25,15 @@ function dataManagement(action, input) {
   }
 
   let file = fs.readFileSync(filePath);
-  
+
   if (action == 'save data' && input != null) {
     if (file.length == 0) {
       fs.writeFileSync(filePath, JSON.stringify([input], null, 2));
     } else {
       let data = JSON.parse(file.toString());
       let inputExist = [];
-      inputExist  = data.map(d => d.original_url);
-      let check_input = inputExist.includes(input.original_url);     
+      inputExist = data.map(d => d.original_url);
+      let check_input = inputExist.includes(input.original_url);
       if (check_input === false) {
         data.push(input);
         fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
@@ -51,28 +51,28 @@ function dataManagement(action, input) {
 }
 
 function gen_shorturl() {
-  let all_Data   = dataManagement('load data');
-  let min = 1; let max = 1000; 
-  if ( all_Data != undefined && all_Data.length > 0 ) { max = all_Data.length*1000 }
+  let all_Data = dataManagement('load data');
+  let min = 1; let max = 1000;
+  if (all_Data != undefined && all_Data.length > 0) { max = all_Data.length * 1000 }
   else { max = 1000; }
-  let short = Math.ceil(Math.random()* (max - min + 1) + min);
+  let short = Math.ceil(Math.random() * (max - min + 1) + min);
 
   if (all_Data === undefined) { return short; }
   else {
-    let shortExist  = all_Data.map(d => d.short_url);
+    let shortExist = all_Data.map(d => d.short_url);
     let check_short = shortExist.includes(short);
-    if ( check_short ) {gen_shorturl(); } else { return short; }
+    if (check_short) { gen_shorturl(); } else { return short; }
   }
-  
+
 }
 
-app.post('/api/shorturl', (req,res) => {
+app.post('/api/shorturl', (req, res) => {
 
   let input = '', domain = '', param = '', short = 0;
 
   input = req.body.url;
-  if (input === null || input === '') { 
-    return res.json({ error: 'invalid url' }); 
+  if (input === null || input === '') {
+    return res.json({ error: 'invalid url' });
   }
 
   domain = input.match(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/igm);
@@ -85,32 +85,32 @@ app.post('/api/shorturl', (req,res) => {
     }
     else {
       short = gen_shorturl();
-      dict = {original_url : input, short_url : short};
+      dict = { original_url: input, short_url: short };
       dataManagement("save data", dict);
       return res.json(dict);
     }
   });
 });
 
-app.get('/api/shorturl/:shorturl', (req,res) => {
-  let input    = Number(req.params.shorturl);
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  let input = Number(req.params.shorturl);
   let all_Data = dataManagement('load data');
 
-  let shortExist  = all_Data.map(d => d.short_url);
+  let shortExist = all_Data.map(d => d.short_url);
   let check_short = shortExist.includes(input);
   if (check_short && all_Data != undefined) {
     data_found = all_Data[shortExist.indexOf(input)];
     res.redirect(data_found.original_url);
   }
   else {
-    res.json({data : 'No matching data', short : input, existing : shortExist});
+    res.json({ data: 'No matching data', short: input, existing: shortExist,all_data: all_Data});
   }
 });
 
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
